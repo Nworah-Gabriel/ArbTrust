90ca9fbafb5685b59fb665873af2d4db59152f91
blacdav <davidaniefoik@gmail.com>
2023-12-20 22:51:28 +0100

    pushing now

---
+++ src/app/Dashboard/Body/Creators.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Creator } from '@/app/ul/Navs'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Creators = () => {
+  return (
+    <div className='lg:mr-10 h-full'>
+      <div className='m-10'>
+        <h2 className='text-[1.5rem] font-bold'>Featured Creators</h2>
+      </div>
+      <div className='grid grid-cols-[repeat(auto-fit,_minmax(30rem,_1fr))] gap-x-7 gap-y-3 justify-center ml-10 lg:ml-0 mr-10 lg:mr-0'>
+      {Creator.map((creator, index) => (
+          <div key={index} className=' mb-4 ml-10 rounded-md px-6 py-4 bg-white shadow-lg flex justify-between'>
+            <div className='flex'>
+            <div>
+            <Image src={creator.img} alt={creator.name} width={80} height={100}/>
+            </div>
+            <div className='grid ml-2 w-full'>
+                <p className='font-bold text-[1.2rem]'>{creator.name}</p>
+                <p className='text-[.8rem] lg:mt-[-1rem]'>{creator.handle}</p>
+                <Link href={'Dashboard/Creators'}><button type='button' className='bg-purple-900 p-2 rounded-md text-white text-[.7rem] lg:text-[1rem] hover:bg-purple-950 hover:shadow-lg hover:shadow-purple-500'>View Collections</button></Link>
+            </div>
+            </div>
+            <div className='flex justify-between w-2/5'>
+                <Image src={creator.nft} alt={creator.name} width={120} height={0} className='rounded-md h-24 '/>
+                <Image src={creator.nft} alt={creator.name} width={80} height={0} className='hidden lg:block rounded-md ml-4 h-24'/>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Creators
---
+++ src/app/Dashboard/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '../globals.css'
+import  Sidebar  from './Sidebar/Sidebar'
+import Header from './Header/Header'
+import MobileSidebar from './Sidebar/MobileSidebar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'DeCertify',
+  description: '',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className='bg-[#f8f8ff] fixed overflow-hidden'>
+      <body className={inter.className}>
+      <div className=' flex'>
+      <Sidebar />
+      <MobileSidebar />
+      <div className='w-full '>
+      <Header />
+        {children}
+        </div>
+        </div>
+      </body>
+    </html>
+  )
+}
