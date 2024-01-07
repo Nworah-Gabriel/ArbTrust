1d0ca52f3cd2706b9fd28fbfb4fdbe33fda47310
blacdav <davidaniefoik@gmail.com>
2024-01-07 18:09:51 +0100

    Added the some modals to the student dashboard

--- src/app/Creator/CreateNFT/page.tsx
+++ src/app/Creator/CreateNFT/page.tsx
@@ -1,71 +1,81 @@
+"use client"
+import Image from 'next/image'
+import { Fragment, useState } from 'react'
+import Modal from '@/components/Modal';
+
 const page = ()=> {
+    const [showModal, setShowModal] = useState<boolean>(false);
     return(
-        <div className='flex flex-col text-black pt-10 lg:p-0'>
-            <div className="w-1/3 mr-5 pt-5 hidden lg:flex self-end">
-                <button className="w-full p-2 mr-2 border-2 border-purple-800 hover:bg-purple-800 text-purple-800 hover:text-white rounded-md">Preview</button>
-                <button className="w-full p-2 bg-purple-800 text-white rounded-md">Publish NFT</button>
-            </div>
-            <section className='bg-white m-5 p-5 rounded-md'>
-                <h1 className="font-bold text-lg ml-2">Create New Item</h1>
-                <div className="flex flex-col lg:flex-row">
-                    <section className="h-[45vh] lg:h-auto flex text-xs flex-col items-center justify-center border-2 border-dashed rounded-md m-2 w-full lg:w-1/2">
-                        <p>Drag and Drop or <span className='text-purple-800 font-bold'>Browse</span></p>
-                        <p>Image(NFTs Assets)</p>
-                        <p className='text-center'>File types supported: JPG, PNG, PDF | Max size: 50MB</p>
-                    </section>
-                    <section className="flex flex-col m-2 w-full lg:w-1/2">
-                        <label htmlFor="NFTname">NFT Name</label>
-                        <input type="text" placeholder='Enter Name' className='p-2 border-2 rounded-md' />
-                        <div className="flex flex-wrap my-4">
-                            <button className='flex items-center justify-center p-2 hover:text-purple-800 my-1 mr-2 border-2 hover:border-purple-800 rounded-md'>Free</button>
-                            <button className='flex items-center justify-center p-2 hover:text-purple-800 my-1 mr-3 border-2 hover:border-purple-800 rounded-md'>Fixed price</button>
-                            <button className='flex items-center justify-center p-2 hover:text-purple-800 my-1 border-2 hover:border-purple-800 rounded-md'>Open For Bid</button>
-                        </div>
-                        <h1 className='mb-1'>Minting Name Positioning</h1>
-                        <div className="flex justify-between">
-                            <button className="flex w-full items-center justify-center p-2 text-purple-800 border-2 rounded-md">A</button>
-                            <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md">B</button>
-                            <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md">C</button>
-                            <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md">D</button>
-                            <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md">E</button>
-                            <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md">F</button>
-                        </div>
-                    </section>
-                </div>
-            </section>
-
-            <h1 className='ml-5 text-xl font-bold'>Questions</h1>
-
-            <section className='flex flex-col lg:flex-row'>
-                <div className='flex flex-col bg-white m-5 mt-2 p-5 rounded-md w-auto lg:w-11/12'>
-                    <h2>Ask Question</h2>
-                    <div className='grid w-24 leading-10 my-3'>
-                        <button>
-                            <input type="radio" name="option" className="mr-2" /> Option
-                        </button>
-                        <button>
-                            <input type="radio" name="option" className="mr-2" /> Option
-                        </button>
-                    </div>
-                    <div className="flex justify-between border-t-2 pt-3">
-                        <section className="flex">
-                            <button>B</button>
-                            <button>I</button>
-                            <button>U</button>
-                        </section>
-                        <section className="flex">
-                            <button>Y</button>
-                            <button>Z</button>
-                        </section>
-                    </div>
-                </div>
-                <button className='bg-purple-800 text-white mr-5 my-5 p-5 rounded-full w-14 h-14 hidden lg:flex items-center justify-center'>+</button>
-                <div className="mx-5 pb-5 flex lg:hidden">
-                    <button className="w-1/2 p-2 mr-2 border-2 border-purple-800 hover:bg-purple-800 text-purple-800 hover:text-white rounded-md">Preview</button>
+        <Fragment>
+            <div className='flex flex-col text-black pt-10 lg:p-0'>
+                <div className="w-1/3 mr-5 pt-5 hidden lg:flex self-end">
+                    <button className="w-full p-2 mr-2 border-2 border-purple-800 hover:bg-purple-800 text-purple-800 hover:text-white rounded-md" onClick = {() => setShowModal(true)}>Preview</button>
                     <button className="w-full p-2 bg-purple-800 text-white rounded-md">Publish NFT</button>
                 </div>
-            </section>
-        </div>
+                <section className='bg-white m-5 p-5 rounded-md'>
+                    <h1 className="font-bold text-lg ml-2">Create New Item</h1>
+                    <div className="flex flex-col lg:flex-row">
+                        <section className="h-[45vh] lg:h-auto flex text-xs flex-col items-center justify-center border-2 border-dashed rounded-md m-2 w-full lg:w-1/2">
+                            <Image src={'/upload_file.svg'} alt='...' width={20} height={20} className='h-16 w-16 hidden lg:block' />
+                            <p>Drag and Drop or <span className='text-purple-800 font-bold'>Browse</span></p>
+                            <p>Image(NFTs Assets)</p>
+                            <p className='text-center'>File types supported: JPG, PNG, PDF | Max size: 50MB</p>
+                        </section>
+                        <section className="flex flex-col m-2 w-full lg:w-1/2">
+                            <label htmlFor="NFTname">NFT Name</label>
+                            <input type="text" placeholder='Enter Name' className='p-2 border-2 rounded-md' />
+                            <div className="flex flex-wrap my-4 text-xs">
+                                <button className='flex items-center justify-center p-2 hover:text-purple-800 my-1 mr-2 border-2 hover:border-purple-800 rounded-md'><Image src={'/payments.svg'} alt='...' width={8} height={8} className='h-5 w-5 mr-2' /> Free</button>
+                                <button className='flex items-center justify-center p-2 hover:text-purple-800 my-1 mr-3 border-2 hover:border-purple-800 rounded-md'><Image src={'/local_offer.svg'} alt='...' width={10} height={10} className='h-5 w-5 mr-2' /> Fixed price</button>
+                                <button className='flex items-center justify-center p-2 hover:text-purple-800 my-1 border-2 hover:border-purple-800 rounded-md'><Image src={'/people.svg'} alt='...' width={10} height={10} className='h-5 w-5 mr-2' /> Open For Bid</button>
+                            </div>
+                            <h1 className='mb-1'>Minting Name Positioning</h1>
+                            <div className="flex justify-between">
+                                <button className="flex w-full items-center justify-center p-2 text-purple-800 border-2 rounded-md"><Image src={'/align_horizontal_left.svg'} alt='...' height={10} width={10} className={'h-5 w-5'} /></button>
+                                <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md"><Image src={'/align_horizontal_center.svg'} alt='...' height={10} width={10} className={'h-5 w-5'} /></button>
+                                <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md"><Image src={'/align_horizontal_right.svg'} alt='...' height={10} width={10} className={'h-5 w-5'} /></button>
+                                <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md"><Image src={'/align_vertical_top.svg'} alt='...' height={10} width={10} className={'h-5 w-5'} /></button>
+                                <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md"><Image src={'/align_vertical_center.svg'} alt='...' height={10} width={10} className={'h-5 w-5'} /></button>
+                                <button className="flex w-full items-center justify-center p-2 ml-1 lg:ml-2 text-purple-800 border-2 rounded-md"><Image src={'/align_vertical_bottom.svg'} alt='...' height={10} width={10} className={'h-5 w-5'} /></button>
+                            </div>
+                        </section>
+                    </div>
+                </section>
+
+                <h1 className='ml-5 text-xl font-bold'>Questions</h1>
+
+                <section className='flex flex-col lg:flex-row'>
+                    <div className='flex flex-col bg-white m-5 mt-2 p-5 rounded-md w-auto lg:w-11/12'>
+                        <h2>Ask Question</h2>
+                        <div className='grid w-24 leading-10 my-3'>
+                            <button>
+                                <input type="radio" name="option" className="mr-2" /> Option
+                            </button>
+                            <button>
+                                <input type="radio" name="option" className="mr-2" /> Option
+                            </button>
+                        </div>
+                        <div className="flex justify-between border-t-2 pt-3">
+                            <section className="flex">
+                                <button><Image src={'/format_bold.svg'} alt='...' width={10} height={10} className='h-5 w-5 mr-2' /></button>
+                                <button><Image src={'/format_italic.svg'} alt='...' width={10} height={10} className='h-5 w-5 mr-2' /></button>
+                                <button><Image src={'/format_underlined.svg'} alt='...' width={10} height={10} className='h-5 w-5 mr-2' /></button>
+                            </section>
+                            <section className="flex">
+                                <button><Image src={'/Vector(1).svg'} alt='...' width={10} height={10} className='h-5 w-5 mr-2' /></button>
+                                <button><Image src={'/delete.svg'} alt='...' width={10} height={10} className='h-5 w-5' /></button>
+                            </section>
+                        </div>
+                    </div>
+                    <button className='bg-purple-800 text-white mr-5 my-5 p-5 rounded-full w-14 h-14 hidden lg:flex items-center justify-center'><Image src={'/add.svg'} alt='...' width={100} height={100} /></button>
+                    <div className="mx-5 pb-5 flex lg:hidden">
+                        <button className="w-1/2 p-2 mr-2 border-2 border-purple-800 hover:bg-purple-800 text-purple-800 hover:text-white rounded-md" onClick = {() => setShowModal(true)}>Preview</button>
+                        <button className="w-full p-2 bg-purple-800 text-white rounded-md">Publish NFT</button>
+                    </div>
+                </section>
+            </div>
+            <Modal isVisible ={showModal} onClose = {()=> setShowModal(false)} />
+        </Fragment>
     )
 }
 export default page;
---
+++ src/components/Modal.tsx
@@ -0,0 +1,126 @@
+"use client"
+import React from 'react'
+import Image from "next/image"
+import { MintCertificate } from "@/app/ul/Navs"
+
+type propTypes = {
+    isVisible: boolean;
+    onClose: ()=> void;
+    // children: React.ReactNode;
+}
+
+const Modal: React.FC<propTypes> = ({ isVisible, onClose }) => {
+    if (!isVisible) return null;
+
+    const handleClose = (event:any) => {
+        if(event.target.id === "wrapper") onClose();
+    }
+  return (
+    <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center' id="wrapper" onClick={handleClose}>
+      <div className='m-auto h-[90vh] w-[80%] text-black bg-[#EDEDED] rounded-lg'>
+            <form action="#">
+                <div className="flex flex-col-reverse lg:flex-row">
+                    <section className='bg-white m-5 lg:mr-2 w-11/12 lg:w-1/2 p-4 rounded-md mt-5 h-fit'>
+                    <h2 className="text-center mb-5 font-bold text-[14px]">ANSWER THE QUESTIONS BELOW</h2>
+                        <div className='grid leading-8'>
+                            <label htmlFor="q1" className="font-semibold">What is the name of your Tutor</label>
+                            <div>
+                                <input type="radio" name='q1' className="mr-2" /> Great Adams
+                            </div>
+                            <div>
+                                <input type="radio" name='q1' className="mr-2" /> Primidac
+                            </div>
+                            <div>
+                                <input type="radio" name='q1' className="mr-2" /> Jerssec
+                            </div>
+                            <div>
+                            <input type="radio" name='q1' className="mr-2" /> Joshua
+                            </div>
+                        </div>
+                        <div className='grid leading-8 mt-3'>
+                            <label htmlFor="q1" className="font-semibold">What is the name of your Tutor</label>
+                            <div>
+                                <input type="radio" name='q2' className="mr-2" /> Great Adams
+                            </div>
+                            <div>
+                                <input type="radio" name='q2' className="mr-2" /> Primidac
+                            </div>
+                            <div>
+                                <input type="radio" name='q2' className="mr-2" /> Jerssec
+                            </div>
+                            <div>
+                            <input type="radio" name='q2' className="mr-2" /> Joshua
+                            </div>
+                        </div>
+                        <div className='grid leading-8 mt-3'>
+                            <label htmlFor="q1" className="font-semibold">What is the name of your Tutor</label>
+                            <div>
+                                <input type="radio" name='q3' className="mr-2" /> Great Adams
+                            </div>
+                            <div>
+                                <input type="radio" name='q3' className="mr-2" /> Primidac
+                            </div>
+                            <div>
+                                <input type="radio" name='q3' className="mr-2" /> Jerssec
+                            </div>
+                            <div>
+                            <input type="radio" name='q3' className="mr-2" /> Joshua
+                            </div>
+                        </div>
+                    </section>
+                    <div className="flex flex-col m-5 lg:ml-2 w-11/12 lg:w-1/2 p-0">
+                        <section className='bg-white rounded-md p-0'>
+                            {/* <div className='p-2 bg-white rounded-md'> */}
+                            {MintCertificate.map((brouse, index) => (
+                                <div key={index} className=''>
+                                    <div className='flex mx-6 mt-4 h-[40%]'>
+                                        <div className='flex flex-col mx-2 w-full'>
+                                            <p className='font-bold text-[.7rem] opacity-70 w-full flex justify-end mb-1'>{brouse.name}</p>
+                                            <p className='text-[.8rem] lg:mt-[-.5rem] font-bold flex justify-end'>{brouse.handle}</p>
+                                        </div>
+                                        <div className='flex justify-end'>
+                                            <Image src={brouse.img} alt={brouse.handle} width={30} height={100} className='' />
+                                        </div>
+                                    </div>
+                                    <div className='relative trigger p-3 flex justify-center h-1/2' >
+                                    {/* <button type='button' className={`btn absolute justify-center  bg-white bg-opacity-80 p-2 rounded-md text-purple-900 font-bold text-[1rem] hover:shadow-lg hover:bg-opacity-100 hover:shadow-white`}>Mint NFT</button> */}
+                                        <div className="block h-fit">
+                                            <Image src={brouse.nft} alt={brouse.handle} width={500} height={40} className='rounded-md h-[44lvh]' />
+                                            <div className='flex flex-col'>
+                                                <div>
+                                                    <p className='font-bold text-[22px] pt-3'>{brouse.title}</p>
+                                                </div>
+                                                <div className='flex justify-between'>
+                                                    <p className='opacity-70 text-[.8rem]'>Transaction fee</p>
+                                                    <div className='flex'>
+                                                        <Image src={brouse.icon} alt={brouse.handle} width={20} height={10} className='rounded-md ' />
+                                                        <p className='font-bold italic'>{brouse.price}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                                {/* <h2 className='font-bold'>UI/UX CERTIFICATE</h2> */}
+                            {/* </div> */}
+                        </section>
+                        <section className='hidden lg:flex flex-col mt-5'>
+                            <label htmlFor="name" className='font-bold text-[14px]'> Full Name</label>
+                            <input type="text" placeholder="Enter Certificate Display Name" className='p-3 bg-white rounded-md' />
+                            <button className="bg-purple-800 text-white p-3 rounded-md mt-5">Mint Certificate</button>
+                        </section>
+                    </div>
+                </div>
+                <section className='flex flex-col w-11/12 mb-10 mx-5 lg:hidden'>
+                    <label htmlFor="name" className='font-bold text-[14px]'> Full Name</label>
+                    <input type="text" placeholder="Enter Certificate Display Name" className='p-3 bg-white rounded-md' />
+                    <button className="bg-purple-800 text-white p-3 rounded-md mt-5">Mint Certificate</button>
+                </section>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Modal
