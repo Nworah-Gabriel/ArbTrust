40af2f2173e263f70c5ec512db29e94e3574ad45
Nworah Gabriel Chimzuruoke <gabrielnworah6@gmail.com>
2024-01-12 00:59:56 +0100

    Update page.tsx

--- frontend/src/app/Dashboard/DSettings/page.tsx
+++ frontend/src/app/Dashboard/DSettings/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import React from 'react'
 import { Camera } from 'lucide-react'
 
-const page = () => {
+const Page = () => {
     const Pathname = usePathname()
     // const [bar, barHandle] = useState<boolean>(true)
 
