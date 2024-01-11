67875f59175f6f604b23d6420cb3ab0e09ba1e9e
unknown <borisadedayopaul@gmail.com>
2024-01-11 22:58:43 +0100

    finalized the project

--- Decertify_Contracts/hardhat.config.js
+++ Decertify_Contracts/hardhat.config.js
@@ -4,4 +4,10 @@ require('@nomiclabs/hardhat-ethers');
 
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/1df4524b8e964edbb748d10809e0b6eb`,
+      accounts: process.env.PRIVATE_KEY,
+    },
+  },
 };
